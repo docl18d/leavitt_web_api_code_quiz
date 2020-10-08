e1443f4afdabe50836639f36083dad6a62248109
Adrein Leavitt <68399114+docl18d@users.noreply.github.com>
2020-10-08 09:58:19 -0600

    question and answer base completed

---
+++ styles/script.js
@@ -0,0 +1,72 @@
+var questions = [{
+    question: "Inside which HTML element do we put the JavaScript?",
+    answer1: "<js>",
+    answer2: "<javascript>",
+    answer3: "<scripting>",
+    answer4: "<script>",
+    correct: "<script>"
+  },{
+    question: "Who invented JavaScript",
+    answer1: "Douglas Crockford",
+    answer2: "Sheryl Sandberg",
+    answer3: "Brendan Eich",
+    answer4: "Black Rifle Coffee Company",
+    correct: "Brendan Eichh"
+  },{
+    question: "Which one of these is a JavaScript package manager?",
+    answer1: "Node.js",
+    answer2: "TypeScript",
+    answer3: "npm",
+    answer4: "CSS",
+    correct: "npm"
+  },{
+    question: "Which of the following is correct about features of JavaScript?",
+    answer1: "JavaScript is a lightweight, interpreted programming language.",
+    answer2: "JavaScript is designed for creating network-centric applications",
+    answer3: "JavaScript is complementary to and integrated with Java",
+    answer4: "All of the Above",
+    correct: "All of the Above"
+  },{
+    question: "Which of the following is the correct syntax to redirect a url using JavaScript?",
+    answer1: "document.location='http://www.newlocation.com';",
+    answer2: "browser.location='http://www.newlocation.com';",
+    answer3: "navigator.location='http://www.newlocation.com'';",
+    answer4: "window.location='http://www.newlocation.com';",
+    correct: "window.location='http://www.newlocation.com';",
+ 
+    question: "The JavaScript syntax defines two types of values what are they?",
+    answer1: "Fixed and Variable",
+    answer2: "Temporary and Permenant",
+    answer3: "Op[en and Closed",
+    answer4: "Integers and Letters",
+    correct: "Fixed and Variable"
+  },{
+    question: "What are JavaScript Variables?",
+    answer1: "Algorythems for computation",
+    answer2: "Containers for storing data values",
+    answer3: "Placholders for syntax expressions",
+    answer4: "A method of array expression",
+    correct: "Containers for storing data values"
+  },{
+    question: "Which of the following is not a JavaScript operator?",
+    answer1: "Assignment",
+    answer2: "Addition",
+    answer3: "String",
+    answer4: "Boolean",
+    correct: "Boolean"
+  },{
+    question: "How is a JavaScript function defined?",
+    answer1: "Defined with the function keyword, followed by a name, followed by parentheses ()",
+    answer2: "var function = {fname:"John", lname:"Doe", age:25}",
+    answer3: "defined with let and const hoisted to the top of the block, but not initialized",
+    answer4: "All of the Above",
+    correct: "Defined with the function keyword, followed by a name, followed by parentheses ()""
+  },{
+    question: "What do you use to run the same code over and over again, each time with a different value.",
+    answer1: "Booleans",
+    answer2: "Loops",
+    answer3: "Arrays",
+    answer4: "Comparisons",
+    correct: "Loops"
+}];
+
