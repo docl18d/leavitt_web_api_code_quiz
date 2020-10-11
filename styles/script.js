175515dee185ac707243904f9acb5e64a147092f
Adrein Leavitt <68399114+docl18d@users.noreply.github.com>
2020-10-10 21:05:28 -0600

    chenged number of questions.  Final trial of the page as good.

--- styles/script.js
+++ styles/script.js
@@ -34,40 +34,7 @@ var questions = [{
     answer4: "window.location='http://www.newlocation.com';",
     correct: "window.location='http://www.newlocation.com';",
   },{
-    question: "The JavaScript syntax defines two types of values what are they?",
-    answer1: "Fixed and Variable",
-    answer2: "Temporary and Permenant",
-    answer3: "Op[en and Closed",
-    answer4: "Integers and Letters",
-    correct: "Fixed and Variable"
-  },{
-    question: "What are JavaScript Variables?",
-    answer1: "Algorythems for computation",
-    answer2: "Containers for storing data values",
-    answer3: "Placholders for syntax expressions",
-    answer4: "A method of array expression",
-    correct: "Containers for storing data values"
-  },{
-    question: "Which of the following is not a JavaScript operator?",
-    answer1: "Assignment",
-    answer2: "Addition",
-    answer3: "String",
-    answer4: "Boolean",
-    correct: "Boolean"
-  },{
-    question: "How is a JavaScript function defined?",
-    answer1: "Defined with the function keyword, followed by a name, followed by parentheses ()",
-    answer2: "var function = {}",
-    answer3: "defined with let and const hoisted to the top of the block, but not initialized",
-    answer4: "All of the Above",
-    correct: "Defined with the function keyword, followed by a name, followed by parentheses ()",
-  },{
-    question: "What do you use to run the same code over and over again, each time with a different value.",
-    answer1: "Booleans",
-    answer2: "Loops",
-    answer3: "Arrays",
-    answer4: "Comparisons",
-    correct: "Loops"
+   
   }];
   
   // Timer
